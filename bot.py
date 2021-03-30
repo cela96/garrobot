34e3da17ee72b6ba4da7f5b0230c0c6590e3afd3
alex313962 <alex313962@gmail.com>
2021-03-30 20:58:47 +0200

    first test

--- bot.py
+++ bot.py
@@ -1,26 +1,24 @@
-import discord
-from discord.ext import commands
+# bot.py
 import os
 
-client = commands.Bot(command_prefix=".")
-token = os.getenv("DISCORD_BOT_TOKEN")
+import discord
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+
+client = discord.Client()
 
 @client.event
-async def on_ready() :
-    await client.change_presence(status = discord.Status.idle, activity = discord.Game("Listening to .help"))
-    print("I am online")
+async def on_ready():
+    for guild in client.guilds:
+        if guild.name == GUILD:
+            break
 
-@client.command()
-async def ping(ctx) :
-    await ctx.send(f"🏓 Pong with {str(round(client.latency, 2))}")
+    print(
+        f'{client.user} is connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})'
+    )
 
-@client.command(name="whoami")
-async def whoami(ctx) :
-    await ctx.send(f"You are {ctx.message.author.name}")
-
-@client.command()
-async def clear(ctx, amount=3) :
-    await ctx.channel.purge(limit=amount)
-
-
-client.run(token)
+client.run(TOKEN)
